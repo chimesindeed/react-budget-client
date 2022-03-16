2768d0f7d40b817cf0932e430eede711deb81043
chimesindeed <gratitudeadore@fastmail.com>
2022-03-16 01:51:01 -0400

    TransactionsProvider component wraps app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import App from './App';
+
+import TransactionsProvider from './components/transactions-provider/TransactionsProvider.component'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
 
 const rootElement = document.getElementById('root')
 
 ReactDOM.render(
-    <App />, rootElement
+  <TransactionsProvider>
+    <App />
+  </TransactionsProvider>, rootElement
 );
