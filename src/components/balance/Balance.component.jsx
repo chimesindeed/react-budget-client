ed9f858bb41c77ec44cf006fdeb43932eb297095
chimesindeed <gratitudeadore@fastmail.com>
2022-03-16 08:42:47 -0400

    balance calculated dynamically from state

--- src/components/balance/Balance.component.jsx
+++ src/components/balance/Balance.component.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import TransactionsContext from '../../context/TransactionsContext'
 
 import styles from './balance.styles.module.scss'
 
 const Balance = () => {
-    const total = 500.00
+    const { transactions } = useContext(TransactionsContext);
+    const amounts = transactions.map((transaction)=>{ return transaction.amount})
+
+    const total = amounts.reduce(
+      (acc, item) => { return acc += item}, 0
+    ).toFixed(2);
+
     return (
-      <div className = {styles.balanceContainer}>
+      <div className ={styles.balanceContainer}>
         <h4 id={styles.balance}>Your Balance</h4>
         <h2 id={styles.total}>${total}</h2>
       </div>
