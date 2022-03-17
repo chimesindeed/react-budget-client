496de3847b3f079ac0f07595bec3ed53201793de
chimesindeed <gratitudeadore@fastmail.com>
2022-03-17 19:02:32 -0400

    added budget card to app, added util to format currency

--- src/App.js
+++ src/App.js
@@ -6,26 +6,39 @@ import Balance from './components/balance/Balance.component'
 import GainsAndExpenses from './components/gains-and-expenses/GainsAndExpenses.component'
 import TransactionList from './components/transaction-list/TransactionList.component'
 import AddTransaction from './components/add-transaction/AddTransaction.component'
+import BudgetCard from './components/budget-card/BudgetCard.component'
 
 import styles from './app.styles.module.scss'
 
 function App() {
   return (
     <Container className='my-4'>
+
       <Stack direction='vertical' gap='2' className='mb-4'>
         <div className={styles.topHorizontalContainer}>
 
           <div className={styles.leftSideContainer}>
             <Header />
           </div>
-          <div className={styles.rightSideContainer}>
+          <div className={styles.middleContainer}>
             <Balance />
             <GainsAndExpenses />
             <TransactionList />
             <AddTransaction />
           </div>
+          <div className={styles.rightSideContainer}>
+            <Stack direction='horizontal' className='mb-4 mt-2'>
+              <h3 className='me-auto'>Budgets</h3>
+              <Button variant='primary'>Add Budget</Button>
+            </Stack>
+          <div className={styles.budgetGrid}>
+            <BudgetCard name='Entertainment' amount={200} max={1000}/>
+          </div>
+
+        </div>
         </div>
       </Stack>
+
     </Container>
   );
 }
