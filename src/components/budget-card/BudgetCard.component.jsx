a83ea741d6c24312d3cf57ae3e6d858b896c33b8
chimesindeed <gratitudeadore@fastmail.com>
2022-03-18 00:59:59 -0400

    added custom classNames array to conditionally add classNames to budget card based on amount

--- src/components/budget-card/BudgetCard.component.jsx
+++ src/components/budget-card/BudgetCard.component.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
-import { Card, ProgressBar } from 'react-bootstrap'
+import { Card, ProgressBar, Stack, Button } from 'react-bootstrap'
 import { currencyFormatter } from '../../utils/utils.js'
 
 
-const BudgetCard = ({name, amount, max}) => {
+const BudgetCard = ({name, amount, max, gray}) => {
+  const classNames = []
+  if(amount > max){
+    classNames.push('bg-danger', 'bg-opacity-10')
+  } else if (gray) {
+    classNames.push('bg-light')
+  }
 
   function getProgressBarVariant(amount, max){
     const ratio = amount / max;
@@ -13,7 +19,7 @@ const BudgetCard = ({name, amount, max}) => {
   }
 
   return (
-    <Card>
+    <Card className={ classNames.join(' ')}>
       <Card.Body>
         <Card.Title className='d-flex justify-content-between align-items-baseline fw-normal  mb-3'>
             <div className='me2'>{name}</div>
@@ -25,7 +31,10 @@ const BudgetCard = ({name, amount, max}) => {
             </div>
         </Card.Title>
         <ProgressBar className='rounded-pill' variant={getProgressBarVariant(amount, max)} min={0} max={max} now={amount} />
-       
+          <Stack direction='horizontal' gap='2' className='mt-4'>
+            <Button variant='outline-primary' className='ms-auto'>Add Expense</Button>
+            <Button variant='outline-secondary'>View Expenses</Button>
+          </Stack>
       </Card.Body>
     </Card>
   )
